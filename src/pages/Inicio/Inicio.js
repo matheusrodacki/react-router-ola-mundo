1a712bebee9957597e549e04f0cf5d9f77d43759
Matheus Rodacki <matheus.rodacki@outlook.com>
2024-03-05 08:30:03 -0300

    adiciona posts

--- src/pages/Inicio/Inicio.js
+++ src/pages/Inicio/Inicio.js
@@ -1,9 +1,19 @@
 import Banner from 'components/Banner';
+import styles from './Inicio.module.css';
+import posts from 'json/posts.json';
+import Post from 'components/Post';
 
 export function Inicio() {
   return (
     <main>
       <Banner />
+      <ul className={styles.posts}>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Post post={post} />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
